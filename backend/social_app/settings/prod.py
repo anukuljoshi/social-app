4f58bbc14fe105969785e92851905dcafff865d6
Anukul Joshi <anukuljoshi1@gmail.com>
2022-03-07 18:44:35 +0530

    https settings

--- backend/social_app/settings/prod.py
+++ backend/social_app/settings/prod.py
@@ -29,8 +29,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# SECURE_PROXY_SLL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SLL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
